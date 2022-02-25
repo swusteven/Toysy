233dfcf3caccbf780d7fdd1e05e79bcf37b56930
swusteven <swusteven@yahoo.com>
2022-02-25 17:00:48 -0500

    backend user sign up and session log in are working as expected

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Api::SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
   def create
-    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
     if @user.nil?
       render json: ['Nope. Wrong credentials!'], status: 401
     else
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
 
   namespace :api, defaults: { format: :json } do
-      resources :users    
+      resources :users, only:[:create, :show]
+      resource :session, only: [:create, :destroy]
   end
   
 
