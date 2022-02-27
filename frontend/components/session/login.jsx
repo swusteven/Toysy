af1162de325eeb8d44027f571b2a2f4a36458318
swusteven <swusteven@yahoo.com>
2022-02-27 07:06:19 -0500

    Working on sign in modal and styling

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -29,27 +29,59 @@ class Login extends React.Component{
     const { setModalToClose, modalOpen } = this.props;
     
     return (
-      <div className="session-form">
-        <Modal isOpen={modalOpen} onRequestClose={()=>setModalToClose()}>
-        <h2>Log In!</h2>
-            <form>
-              <label>Email:
+      <div className="session-form-wrapper">
+        <Modal  isOpen={modalOpen} 
+                onRequestClose={()=>setModalToClose()}
+                style={
+                  {
+                    overlay: {
+                      backgroundColor: "rgba(0, 0, 0, .4)",
+                    },
+                    content: {
+                      borderRadius: '24px',
+                      width: "325px",
+                      margin: "auto",
+                      height: "600px",
+                      padding: "40px",
+                      
+                    }
+                    
+                  }
+                }        
+        >
+        
+        <div className="modal-close-xxx">
+          <span onClick={() => setModalToClose(false)}>X</span>
+        </div>
+
+        <div className="session-signin-register-divider">
+          <h2>Sign In</h2>
+          <button className="session-register-btn">Register</button>
+        </div>
+            <form className="session-form">
+              <label>Email address:
+                <br />
                 <input 
                   type="text"
                   value={this.state.email}
                   onChange= {this.handleInput('email')}
                 />
               </label>
+              
+              <br />
+
               <label>Password:
+                <br />
                 <input 
                   type="password"
                   value={this.state.password}
                   onChange= {this.handleInput('password')}
                 />
               </label>
-              <button onClick={this.handleSubmit}>Log In</button>
+              <br />
+              <button className="signin-btn" onClick={this.handleSubmit}>Sign In</button>
 
-              <button onClick={() => setModalToClose(false)}>Close</button>
+              
             </form>
         </Modal>
       </div>
