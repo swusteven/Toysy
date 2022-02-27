bc043887f804c6a982990b98734d076454d244c2
swusteven <swusteven@yahoo.com>
2022-02-26 20:55:07 -0500

    Complete User Auth

--- frontend/components/nav_bar/account_manager.jsx
+++ frontend/components/nav_bar/account_manager.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const AccountManager = ({ currentUser, logout }) => {
-  debugger
   const display = currentUser ? (
     <div>
       <p>Hello, {currentUser.fname}</p>
