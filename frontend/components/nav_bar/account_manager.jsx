d0acb2d86c2260ad99b8df98d587889e32e55483
swusteven <swusteven@yahoo.com>
2022-03-05 10:40:50 -0500

    complete dropdown menu

--- frontend/components/nav_bar/account_manager.jsx
+++ frontend/components/nav_bar/account_manager.jsx
@@ -9,13 +9,19 @@ const AccountManager = ({ currentUser, logout, setModalToOpen }) => {
   // }
   
   const display = currentUser ? (
-    <div className="user-menu-dropdown-main">
-        <div className="user-menu-account-entry-main">
+    <ul className="user-menu-main">
+      <li className="user-menu-entry">
           <i className="fa-solid fa-circle-user fa-2x"></i>
-          <i className="fa-solid fa-sort-down 2x"></i>
+          <i className="fa-solid fa-sort-down 2x"></i></li>
+      <div className="user-menu-dropdown" >
+          <div> <i className="fa-solid fa-circle-user fa-2x"></i>   {currentUser.fname}</div>
+          <p className="user-menu-clicks-orders"><i className="fa-solid fa-list-check fa-2x"></i>Purchases and Reviews</p>
+          <p className="user-menu-clicks-logout" onClick={ logout }><i class="fa-solid fa-right-from-bracket fa-2x fa-flip-horizontal"></i>Log out</p>
       </div>
-      <button onClick={ logout }>Log out</button>
-    </div>
+      
+    </ul>
+
+    
   ) :(
     <div>
       <Link className="signin-link" onClick={()=>setModalToOpen()}to="/login" >Sign In</Link>
