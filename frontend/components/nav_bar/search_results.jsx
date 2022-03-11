9f51bf760246383dbc4a93bfc2df094e8dbd57e9
swusteven <swusteven@yahoo.com>
2022-03-11 01:25:13 -0500

    Complete Search

--- frontend/components/nav_bar/search_results.jsx
+++ frontend/components/nav_bar/search_results.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { searchQuery } from '../../utils/searchQuery'
+import { Link } from 'react-router-dom'
 
 class SearchResults extends React.Component{
   constructor(props){
@@ -12,19 +13,32 @@ class SearchResults extends React.Component{
 
   render(){
     const {searchValue, products} = this.props
-    // let results = searchQuery(this.props.search, this.props.products) 
     return (
-      !searchValue ? null :
-      <section>
-        {
-          Object.values(searchQuery(searchValue, products)).map(product =>(
-              <h1>{product.name}</h1>
-          ))
-            
+      !searchValue ? <h6 className="search-result-no-match">No Match. Please try again with different search key word(s)</h6> :
+      <>
+           <section className='search-result-top'>
+             <div>
+               <h1>{Object.values(searchQuery(searchValue, products)).length === 0 ? 0 : Object.values(searchQuery(searchValue, products)).length} Results Found</h1>   
+            </div>
+          </section> 
+          <section className='search-result-wrapper'>
+
+            {
+              Object.values(searchQuery(searchValue, products)).map(product =>{
+                  return <div key={product.name} className="search-results-container">
+                                <Link to={`/products/${product.id}`}>
+                                    <img src={product.imageUrl} />
+                                    <h2>{product.name}</h2>
+                                    <p>${product.price}</p>
+                                    <p>{product.seller.fname}'s store</p> 
+                                  </Link>
+                              </div>
+                               })
           
 
-       }
-      </section>
+             }
+          </section>
+      </>
     )
   }
 }
