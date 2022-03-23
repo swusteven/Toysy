42fbd18e45e8db817e12db3c3385582ddeb39873
swusteven <swusteven@yahoo.com>
2022-03-22 21:59:16 -0400

    optimized search query

--- frontend/utils/searchQuery.js
+++ frontend/utils/searchQuery.js
@@ -1,35 +1,20 @@
 export const searchQuery =(searchValue, products) =>{
-  if (!searchValue){
-    return []
-  }
-
+  if (!searchValue) return [];
+    
   let productsInArray = Array.from(Object.values(products));
   let searchValueInArray = searchValue.split(" ");
 
-  let results = [];
+  const results = new Set();
 
   productsInArray.forEach(product=>{
     let productName = product.name.toLowerCase()
     for (let i = 0; i < searchValueInArray.length; i++) {
           if (productName.includes(searchValueInArray[i].toLowerCase())){
-              results.push(product)
-            continue
+              results.add(product)
+              continue
           }
     }   
   });
 
-  if (results.length === 0 ) return [];
-
-  let visited = [];
-  let finalResults = [];
-  
- for (let i = 0; i < results.length; i++) {
-    if (!visited.includes(results[i].id)){
-      visited.push(results[i].id)
-      finalResults.push(results[i])
-    }   
- }
-
-  return finalResults
-
+  return Array.from(results)
 }
