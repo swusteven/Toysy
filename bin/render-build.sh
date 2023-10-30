e6dc97f54d72f5b8885f80a6bab60f6ce0cf8c50
swusteven <swusteven@yahoo.com>
2023-10-30 14:45:30 -0400

    updated render_build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,12 @@
 set -o errexit
 
 bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 
 npm install
 
 #if you have seeds to run add:
 bundle exec rails db:setup
+
+npm start
